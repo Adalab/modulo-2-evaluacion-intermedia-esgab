6828aa577fdd7148ad0602d0611f8a94b7148980
Esther García <garbennes@gmail.com>
2023-12-09 09:11:05 +0100

    Add main.js and query selectors

---
+++ main.js
@@ -0,0 +1,4 @@
+const gameInput = document.querySelector(".js-input-number");
+const playButton = document.querySelector(".js-play-button");
+const gameTip = document.querySelector(".js-game-tip");
+const gameTry = document.querySelector(".js-game-try");
